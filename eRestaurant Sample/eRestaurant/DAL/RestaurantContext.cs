7f7f9f2f9cd8df5cf3947560806cc9feef98c239
acuevas1 <yheen.cuevas@gmail.com>
2014-09-12 09:43:40 -0600

    added classes for eRestaurant and Restaurant Context

---
+++ eRestaurant Sample/eRestaurant/DAL/RestaurantContext.cs
@@ -0,0 +1,47 @@
+﻿using eRestaurant.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity; // needed for DBContext
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.DAL
+{
+    public class RestaurantContext : DbContext // can make it internal
+    {
+
+        #region Constructor
+
+        public RestaurantContext() : base("name=EatIn") { }
+
+        #endregion
+
+        #region Properties for the table-to-entity mappings
+       
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<SpecialEvent> SpecialEvents { get; set; }
+        public DbSet<Table> Tables { get; set; }
+        
+        #endregion
+
+
+        #region Over-rideCreating
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                //many to many
+                .Entity<Reservation>().HasMany(r => r.Tables)
+                .WithMany(t => t.Reservations)
+                .Map(mapping =>
+                {
+                    mapping.ToTable("ReservationTables");
+                    mapping.ToTable("ReservationID");
+                    mapping.ToTable("TableID");
+
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+        #endregion
+    }
+}
---
+++ eRestaurant Sample/eRestaurant/Entities/Table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities
+{
+    public class Table
+    {
+        // In EF, by convention, if there is a property with a same name as the class but with the suffix ID, then any framework will assume that this property is mapping to a primary key column on the database table.
+
+        //You can still add the KEY but it is not necessary
+        public int TableID { get; set; }
+        //if table number is the primary key, put the [KEY] here.
+        public byte TableNumber { get; set; }
+        public bool Smoking { get; set; }
+        public int Capacity { get; set; }
+        public bool Available { get; set; }
+
+        #region Navigation Properties
+        //build a rich object model for a table class
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+
+        #endregion  
+    }
+}
