4f3466e54c074d7957137c7b1eeb0dab4f46abd8
acuevas1 <yheen.cuevas@gmail.com>
2014-09-19 09:47:33 -0600

    scripts and manage waiters

--- eRestaurant Sample/eRestaurant.Requirements/UserStories/ManageWaitersStory.cs
+++ eRestaurant Sample/eRestaurant.Requirements/UserStories/ManageWaitersStory.cs
@@ -1,4 +1,5 @@
-﻿using eRestaurant.Entities;
+﻿using eRestaurant.BLL;
+using eRestaurant.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,56 +9,51 @@ using TestStack.BDDfy;
 using Xunit;
 using Xunit.Extensions;
 
-
 namespace eRestaurant.Requirements.UserStories
 {
     [Story(AsA = "Restaurant Manager",
-            IWant = "To manage my waiters",
-            SoThat = "I have enough staff to wait on tables")]
+           IWant = "To manage my waiters",
+           SoThat = "I have enough staff to wait on tables")]
     public class ManageWaitersStory
     {
         [Fact]
         [AutoRollback]
-        public void AddWaiterScenario() 
+        public void AddWaiterScenario()
         {
-
             Waiter newGuy = new Waiter()
             {
-                FirstName="Fred",
+                FirstName = "Fred",
                 LastName = "Flintstone",
-                Address ="123 Bedrock",
-                Phone="780.555.1212"
+                Address = "123 Bedrock",
+                Phone = "780.555.1212",
+                HireDate = DateTime.Today.AddDays(-3)
             };
-            int waiterId=-1;
+            int waiterId = -1;
             this.Given(_ => GivenWaiterInformation(newGuy))
-                .When(_ =>WhenIAddTheWaiter (newGuy, out waiterId))
-                .Then (_=> ThenTheWaiterExists(waiterId ))
-                    .And(_ =>TheWaiterDetailsMatch(waiterId,newGuy))
-                    .BDDfy();
-
-                   
+                .When(_ => WhenIAddTheWaiter(newGuy, out waiterId))
+                .Then(_ => ThenTheWaiterExists(waiterId))
+                .And(_ => TheWaiterDetailsMatch(waiterId, newGuy))
+                .BDDfy();
         }
 
         private void TheWaiterDetailsMatch(int waiterId, Waiter newGuy)
         {
-            var sut = new eRestaurant.BLL.RestaurantAdminController();
-            //to do: compare actual waiter info with the expected
+            var sut = new RestaurantAdminController();
+            // TODO: Compare the actual waiter info with the expected
         }
 
         private void ThenTheWaiterExists(int waiterId)
         {
-            var sut = new eRestaurant.BLL.RestaurantAdminController();
+            var sut = new RestaurantAdminController();
             var actual = sut.GetWaiter(waiterId);
             Assert.NotNull(actual);
         }
 
         private void WhenIAddTheWaiter(Waiter newGuy, out int waiterId)
         {
-            //sut- system under test
-
-            var sut = new eRestaurant.BLL.RestaurantAdminController();
+            // sut ==> "system under test"
+            var sut = new RestaurantAdminController();
             waiterId = sut.AddWaiter(newGuy);
-
         }
 
         private void GivenWaiterInformation(Waiter newGuy)
@@ -67,7 +63,5 @@ namespace eRestaurant.Requirements.UserStories
             Assert.False(string.IsNullOrEmpty(newGuy.Address));
             Assert.False(string.IsNullOrEmpty(newGuy.Phone));
         }
-
-
     }
 }
