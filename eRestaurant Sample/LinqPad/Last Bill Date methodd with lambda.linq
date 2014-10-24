f3cff355dd3ef89601d75ce2405f65ebb72e85e7
acuevas1 <yheen.cuevas@gmail.com>
2014-10-24 09:53:49 -0600

    ^.^

---
+++ eRestaurant Sample/LinqPad/Last Bill Date methodd with lambda.linq
@@ -0,0 +1,35 @@
+<Query Kind="Program">
+  <Connection>
+    <ID>fc618045-c805-4cee-a07a-a91c7cce7297</ID>
+    <Persist>true</Persist>
+    <Server>.</Server>
+    <Database>eRestaurant</Database>
+  </Connection>
+</Query>
+
+//Bills.Max(x => x.BillDate); // lambda syntax C# Expression
+
+void Main()
+{
+	//use lambda expression to get the maximum
+	//  A lambda is simply a shorthand version of a function call
+	// that is ideal for anonymous delegates
+	Bills.Max(x => x.BillDate).Dump();
+		// => such that
+	
+	
+	//	 longer version using an actual method name
+	// 	that we pass in to the Max() Methid
+	//	Note that the Max() method is overloaded, therefore we need to specify
+	// 	in the generic identifier of the method
+	// 	which version we are using
+	Bills.Max < Bills, DateTime > (GetProperty).Dump();
+
+}
+
+//define other methods and classes here
+
+private DateTime GetProperty(Bills x)
+{
+	return x.BillDate;
+}
---
+++ eRestaurant Sample/eRestaurant/Entities/DTOs/Order.cs
@@ -0,0 +1,35 @@
+﻿using eRestaurant.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities.DTOs
+{
+    public class Order
+    {
+        public int TableNumber { get; set; }
+        public string Waiter { get; set; }
+        public int WaiterID { get; set; }
+        public int? BillID { get; set; }
+        public bool Served { get; set; }
+        public string OrderComments { get; set; }
+        //you do: Create a prop called TotalAmount which will sum up the item totals in the Items collection
+        //remember to check if Items is null. Use a LINQ method .Sum() with a lambda to get the total
+
+        public decimal TotalAmount 
+        {
+            get
+            {
+                using (var context = new RestaurantContext())
+                {
+                    var result = context. Items.Sum(x => x.CurrentPrice);
+                    return result;
+                }
+            }
+        }
+
+        public IList<OrderItem> Items { get; set; }
+    }
+}
